7aaac79a4d84e883ec91a2c364549ac46cc35abf
NLferdiNL <nlferdinl@gmail.com>
2016-04-11 15:56:33 +0200

    XInput final push
    also containing a test script i made.. for science

---
+++ Assets/Placeholder/ControllerTest.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ControllerTest : MonoBehaviour {
+
+    XInput xinput;
+
+	void Start () {
+        xinput = GetComponent<XInput>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        print(xinput.LeftStickPos);
+	}
+}
--- Assets/Scripts/XInput.cs
+++ Assets/Scripts/XInput.cs
@@ -15,19 +15,77 @@ public class XInput : MonoBehaviour {
     // Only true this frame.
     bool xButtonDown, bButtonDown, yButtonDown, aButtonDown = false;
     bool startButtonDown, backButtonDown = false;
-    bool leftstickDown, rightStickDown = false;
+    bool leftStickDown, rightStickDown = false;
+    bool leftShoulderDown, rightShoulderDown = false;
+    bool dPadLeftDown, dPadRightDown, dPadUpDown, dPadDownDown = false;
+
+    public bool XButtonDown { get { return xButtonDown; } }
+    public bool BButtonDown { get { return bButtonDown; } }
+    public bool YButtonDown { get { return yButtonDown; } }
+    public bool AButtonDown { get { return aButtonDown; } }
+
+    public bool StartButtonDown { get { return startButtonDown; } }
+    public bool BackButtonDown { get { return backButtonDown; } }
+
+    public bool LeftStickDown { get { return leftStickDown; } }
+    public bool RightStickDown { get { return rightStickDown; } }
+
+    public bool LeftShoulderDown { get { return leftShoulderDown; } }
+    public bool RightShoulderDown { get { return rightShoulderDown; } }
+
+    public bool DPadLeftDown { get { return dPadLeftDown; } }
+    public bool DPadRightDown { get { return dPadRightDown; } }
+    public bool DPadUpDown { get { return dPadUpDown; } }
+    public bool DPadDownDown { get { return dPadDownDown; } }
 
     // True when button is down.
     bool xButton, bButton, yButton, aButton = false;
     bool startButton, backButton = false;
-    bool leftstick, rightStick = false;
+    bool leftStick, rightStick = false;
+    bool leftShoulder, rightShoulder = false;
+    bool dPadLeft, dPadRight, dPadUp, dPadDown = false;
+
+    public bool XButton { get { return xButton; } }
+    public bool BButton { get { return bButton; } }
+    public bool YButton { get { return yButton; } }
+    public bool AButton { get { return aButton; } }
+
+    public bool StartButton { get { return startButton; } }
+    public bool BackButton { get { return backButton; } }
+
+    public bool LeftStick { get { return leftStick; } }
+    public bool RightStick { get { return rightStick; } }
+
+    public bool LeftShoulder { get { return leftShoulder; } }
+    public bool RightShoulder { get { return rightShoulder; } }
+
+    public bool DPadLeft { get { return dPadRight; } }
+    public bool DPadRight { get { return dPadRight; } }
+    public bool DPadUp { get { return dPadUp; } }
+    public bool DPadDown { get { return dPadDown; } }
 
     // Their horizontal and vertical.
     // Value between -1 and 1.
     Vector2 leftStickPos, rightStickPos;
+    float leftShoulderPos, rightShoulderPos;
 
-    [SerializeField, Range(0,1)]
-    float Vibration = 0;
+    public Vector2 LeftStickPos { get { return leftStickPos; } }
+    public Vector2 RightStickPos { get { return rightStickPos; } }
+
+    public float LeftShoulderPos { get { return leftShoulderPos; } }
+    public float RightShoulderPos { get { return RightShoulderPos; } }
+
+    public float LeftMotorVibration {
+        set {
+            SetVib(value, 0);
+        }
+    }
+
+    public float RightMotorVibration {
+        set {
+            SetVib(0, value);
+        }
+    }
 
     void Start() {
         switch (playerNo) {
@@ -45,7 +103,7 @@ public class XInput : MonoBehaviour {
                 break;
             default:
                 Debug.LogError(this.name + " was not given a player number and therefore cannot continue!");
-                gameObject.name = "!--" + gameObject.name + "--!";
+                gameObject.name = "!!!--" + gameObject.name + "--!!!"; // Can never be too careful.
                 gameObject.SetActive(false);
                 break;
         }
@@ -54,27 +112,80 @@ public class XInput : MonoBehaviour {
     }
 
     void Update() {
-        PressedThisFrame();
+        lastState = currentState;
+        currentState = GamePad.GetState(playerIndex);
+
+        ButtonReleaseFrame();
+        ButtonPressFrame();
+
+        ButtonPress();
+
+        Positions();
     }
 
-    void PressedThisFrame() {
+    void ButtonPressFrame() { // Buttons pressed this frame.
         xButtonDown = lastState.Buttons.X == ButtonState.Released && currentState.Buttons.X == ButtonState.Pressed;
         bButtonDown = lastState.Buttons.B == ButtonState.Released && currentState.Buttons.B == ButtonState.Pressed;
         yButtonDown = lastState.Buttons.Y == ButtonState.Released && currentState.Buttons.Y == ButtonState.Pressed;
         aButtonDown = lastState.Buttons.A == ButtonState.Released && currentState.Buttons.A == ButtonState.Pressed;
         startButtonDown = lastState.Buttons.Start == ButtonState.Released && currentState.Buttons.Start == ButtonState.Pressed;
         backButtonDown = lastState.Buttons.Back == ButtonState.Released && currentState.Buttons.Back == ButtonState.Pressed;
-        leftstickDown = lastState.Buttons.LeftStick == ButtonState.Released && currentState.Buttons.LeftStick == ButtonState.Pressed;
-        rightStickDown = lastState.Buttons.RightStick == ButtonState.Released && currentState.Buttons.RightStick == ButtonState.Pressed;
+        leftStickDown = lastState.Buttons.LeftStick == ButtonState.Released && currentState.Buttons.LeftStick == ButtonState.Pressed;
+        rightShoulderDown = lastState.Buttons.RightShoulder == ButtonState.Released && currentState.Buttons.RightShoulder == ButtonState.Pressed;
+        leftShoulderDown = lastState.Buttons.LeftShoulder == ButtonState.Released && currentState.Buttons.LeftShoulder == ButtonState.Pressed;
 
-        xButtonDown = !(lastState.Buttons.X == ButtonState.Pressed && currentState.Buttons.X == ButtonState.Released);
-        bButtonDown = !(lastState.Buttons.B == ButtonState.Pressed && currentState.Buttons.B == ButtonState.Released);
-        yButtonDown = !(lastState.Buttons.Y == ButtonState.Pressed && currentState.Buttons.Y == ButtonState.Released);
-        aButtonDown = !(lastState.Buttons.A == ButtonState.Pressed && currentState.Buttons.A == ButtonState.Released);
-        startButtonDown = !(lastState.Buttons.Start == ButtonState.Pressed && currentState.Buttons.Start == ButtonState.Released);
-        backButtonDown = !(lastState.Buttons.Back == ButtonState.Pressed && currentState.Buttons.Back == ButtonState.Released);
-        leftstickDown = !(lastState.Buttons.LeftStick == ButtonState.Pressed && currentState.Buttons.LeftStick == ButtonState.Released);
-        rightStickDown = !(lastState.Buttons.RightStick == ButtonState.Pressed && currentState.Buttons.RightStick == ButtonState.Released);
+        dPadLeftDown = lastState.DPad.Left == ButtonState.Released && currentState.DPad.Left == ButtonState.Pressed;
+        dPadRightDown = lastState.DPad.Right == ButtonState.Released && currentState.DPad.Right == ButtonState.Pressed;
+        dPadUpDown = lastState.DPad.Up == ButtonState.Released && currentState.DPad.Up == ButtonState.Pressed;
+        dPadDownDown = lastState.DPad.Down == ButtonState.Released && currentState.DPad.Down == ButtonState.Pressed;
+    }
+
+    void ButtonReleaseFrame() { // Buttons released this frame.
+        xButtonDown = !(lastState.Buttons.X == ButtonState.Pressed && currentState.Buttons.X == ButtonState.Pressed);
+        bButtonDown = !(lastState.Buttons.B == ButtonState.Pressed && currentState.Buttons.B == ButtonState.Pressed);
+        yButtonDown = !(lastState.Buttons.Y == ButtonState.Pressed && currentState.Buttons.Y == ButtonState.Pressed);
+        aButtonDown = !(lastState.Buttons.A == ButtonState.Pressed && currentState.Buttons.A == ButtonState.Pressed);
+        startButtonDown = !(lastState.Buttons.Start == ButtonState.Pressed && currentState.Buttons.Start == ButtonState.Pressed);
+        backButtonDown = !(lastState.Buttons.Back == ButtonState.Pressed && currentState.Buttons.Back == ButtonState.Pressed);
+        leftStickDown = !(lastState.Buttons.LeftStick == ButtonState.Pressed && currentState.Buttons.LeftStick == ButtonState.Pressed);
+        rightStickDown = !(lastState.Buttons.RightStick == ButtonState.Pressed && currentState.Buttons.RightStick == ButtonState.Pressed);
+        rightShoulderDown = !(lastState.Buttons.RightShoulder == ButtonState.Pressed && currentState.Buttons.RightShoulder == ButtonState.Pressed);
+        leftShoulderDown = !(lastState.Buttons.LeftShoulder == ButtonState.Pressed && currentState.Buttons.LeftShoulder == ButtonState.Pressed);
+
+        dPadLeftDown = !(lastState.DPad.Left == ButtonState.Pressed && currentState.DPad.Left == ButtonState.Pressed);
+        dPadRightDown = !(lastState.DPad.Right == ButtonState.Pressed && currentState.DPad.Right == ButtonState.Pressed);
+        dPadUpDown = !(lastState.DPad.Up == ButtonState.Pressed && currentState.DPad.Up == ButtonState.Pressed);
+        dPadDownDown = !(lastState.DPad.Down == ButtonState.Pressed && currentState.DPad.Down == ButtonState.Pressed);
+    }
+
+    void ButtonPress() {
+        xButton = currentState.Buttons.X == ButtonState.Pressed;
+        bButton = currentState.Buttons.B == ButtonState.Pressed;
+        yButton = currentState.Buttons.Y == ButtonState.Pressed;
+        aButton = currentState.Buttons.A == ButtonState.Pressed;
+        startButton = currentState.Buttons.Start == ButtonState.Pressed;
+        backButton = currentState.Buttons.Back == ButtonState.Pressed;
+        leftStick = currentState.Buttons.LeftStick == ButtonState.Pressed;
+        rightStick = currentState.Buttons.RightStick == ButtonState.Pressed;
+        rightShoulder = currentState.Buttons.RightShoulder == ButtonState.Pressed;
+        leftShoulder = currentState.Buttons.LeftShoulder == ButtonState.Pressed;
+
+        dPadLeft = currentState.DPad.Left == ButtonState.Pressed;
+        dPadRight = currentState.DPad.Right == ButtonState.Pressed;
+        dPadUp = currentState.DPad.Up == ButtonState.Pressed;
+        dPadDown = currentState.DPad.Down == ButtonState.Pressed;
+    }
+
+    void Positions() {
+        // Just some shortcuts.
+        GamePadThumbSticks currentTS = currentState.ThumbSticks;
+        GamePadTriggers currentT = currentState.Triggers;
+
+        leftStickPos.x = currentTS.Left.X; leftStickPos.y = currentTS.Left.Y;
+        rightStickPos.x = currentTS.Right.X; rightStickPos.y = currentTS.Right.Y;
+
+        leftShoulderPos = currentT.Left;
+        rightShoulderPos = currentT.Right;
     }
 
     public void SetVib(float leftMotor, float rightMotor = -2) {
