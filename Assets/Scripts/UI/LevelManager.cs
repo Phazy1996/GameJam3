bb1112c02a15a1567b789b4bece0766df13453a3
DBeumans <buncho.gamingnetwork@gmail.com>
2016-04-11 12:28:08 +0200

    Added Main Scene, Added simple main menu with play and quit button, play button change to scene one ( can change in unity editor )

---
+++ Assets/Scripts/UI/LevelManager.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+
+    
+    public void StartLevel(int Scene)
+    {
+        SceneManager.LoadScene(Scene);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
