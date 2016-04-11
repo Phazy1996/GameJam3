6b381ef9f411078aeedc45d014bb796b0ad87673
DBeumans <buncho.gamingnetwork@gmail.com>
2016-04-11 14:48:59 +0200

    Added simple endgame UI in game scene, added credits button

--- Assets/Scripts/UI/LevelManager.cs
+++ Assets/Scripts/UI/LevelManager.cs
@@ -4,12 +4,24 @@ using System.Collections;
 
 public class LevelManager : MonoBehaviour {
 
-    
+
+    public GameObject CreditsUI;
     public void StartLevel(int Scene)
     {
         SceneManager.LoadScene(Scene);
     }
 
+    public void CreditsShow(bool value)
+    {
+        if(value)
+        {
+            CreditsUI.SetActive(true);
+        }
+        else
+        {
+            CreditsUI.SetActive(false);
+        }
+    }
     public void QuitGame()
     {
         Application.Quit();
