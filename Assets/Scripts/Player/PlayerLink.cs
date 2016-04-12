9b51e18aac32bc7b22c3d0386d86867204ba7d89
Phineas Moore <phineasmoore@gmail.com>
2016-04-12 10:28:41 +0200

    commit

--- Assets/Scripts/Player/PlayerLink.cs
+++ Assets/Scripts/Player/PlayerLink.cs
@@ -3,7 +3,11 @@ using System.Collections;
 
 public class PlayerLink : MonoBehaviour {
 
+    [SerializeField]
     private Rigidbody2D rb2D;
+    [SerializeField]
+    private DistanceJoint2D dis2D;
+    private float distance;
 
     [SerializeField]
     private Transform player1;
@@ -14,13 +18,20 @@ public class PlayerLink : MonoBehaviour {
     [SerializeField]
     private Transform player4;
 
+    [SerializeField]
+    private GameObject [] team1;
+    [SerializeField]
+    private GameObject [] team2;
+
 	// Use this for initialization
 	void Start () {
-	
+        team1[2];
+        team2[2];
 	}
 	
 	// Update is called once per frame
 	void FixedUpdate () {
-
+        dis2D.distance = 3f;
+       // dis2D.distance = dis2D.maxDistanceOnly;
 	}
 }
