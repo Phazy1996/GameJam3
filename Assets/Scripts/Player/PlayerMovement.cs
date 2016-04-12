65af339ecae4158115cae76ce05b7af44d9c9ebd
DBeumans <buncho.gamingnetwork@gmail.com>
2016-04-12 11:54:06 +0200

    Commit Danilo

---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using XInputDotNetPure;
+
+public class PlayerMovement : MonoBehaviour {
+
+    float movementSpeed =10;
+    XInput _XInput;
+
+	// Use this for initialization
+	void Start ()
+    {
+        _XInput = GetComponent<XInput>();
+	}
+	
+	// FixedUpdate is called once per frame
+    void FixedUpdate()
+    {
+        if(_XInput.LeftStickPos.x > 0.2)
+        {
+            transform.position = new Vector2(movementSpeed, 0f);
+        }
+        if(_XInput.LeftStickPos.x < -0.2)
+        {
+            
+        }
+
+    }
+}
