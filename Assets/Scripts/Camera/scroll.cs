586bf9c625634f50beae5ebdabc510dd64749ad6
ninoosewoudt <ninoosewoudt@Ninos-MacBook-Pro.local>
2016-04-12 12:28:02 +0200

    parallax

---
+++ Assets/Scripts/Camera/scroll.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class scroll : MonoBehaviour {
+
+	[SerializeField]
+	private float scrollSpeed = 100f;
+	private float parralax = 2f;
+
+	void Update () {
+	    
+		MeshRenderer mr = GetComponent<MeshRenderer> ();
+
+		Material mat = mr.material;
+
+		Vector2 offset = mat.mainTextureOffset;
+
+		offset.x += Time.deltaTime / scrollSpeed;
+
+		mat.mainTextureOffset = offset;
+
+	}
+}
