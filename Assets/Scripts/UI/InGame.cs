2e31717a4d854d34481ffd438bd7fae68fe3cd74
NLferdiNL <nlferdinl@gmail.com>
2016-04-12 15:36:45 +0200

    junk clean and collision fix

--- Assets/Scripts/UI/InGame.cs
+++ Assets/Scripts/UI/InGame.cs
@@ -4,18 +4,15 @@ using UnityEngine.UI;
 public class InGame : MonoBehaviour {
 
     [SerializeField]
-    Text scoreText, timeText;
+    Text timeText;
 
-    Score score;
     Timer timer;
 
     void Start() {
-        score = GetComponent<Score>();
         timer = GetComponent<Timer>();
     }
 
     void Update() {
-        scoreText.text = score.Scoreboard();
         timeText.text = timer.CurrentTimeString();
     }
 
