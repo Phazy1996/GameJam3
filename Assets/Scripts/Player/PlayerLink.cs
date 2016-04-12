9333f779bf5b23440d9f43a5bc6d70936a97bd59
Phineas Moore <phineasmoore@gmail.com>
2016-04-12 11:17:18 +0200

    NO PLAYERLINK.CS

--- Assets/Scripts/Player/PlayerLink.cs
+++ Assets/Scripts/Player/PlayerLink.cs
@@ -3,35 +3,31 @@ using System.Collections;
 
 public class PlayerLink : MonoBehaviour {
 
-    [SerializeField]
     private Rigidbody2D rb2D;
-    [SerializeField]
     private DistanceJoint2D dis2D;
-    private float distance;
+    private GameObject teamMate;
+//    private float distance;
+
+//    [SerializeField]
+//    private Transform player1;
+//    [SerializeField]
+//    private Transform player2;
+//    [SerializeField]
+//    private Transform player3;
+//   [SerializeField]
+//    private Transform player4;
 
     [SerializeField]
-    private Transform player1;
-    [SerializeField]
-    private Transform player2;
-    [SerializeField]
-    private Transform player3;
-    [SerializeField]
-    private Transform player4;
-
-    [SerializeField]
-    private GameObject [] team1;
-    [SerializeField]
-    private GameObject [] team2;
+    private GameObject [] team;
 
 	// Use this for initialization
 	void Start () {
-       // team1[2];
-      //  team2[2];
+
 	}
 	
 	// Update is called once per frame
 	void FixedUpdate () {
-        dis2D.distance = 3f;
-       // dis2D.distance = dis2D.maxDistanceOnly;
+       // dis2D.distance = 3f;
+        //dis2D.Game
 	}
 }
